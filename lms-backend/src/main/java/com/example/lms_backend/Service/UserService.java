50f4168fb16e9bb2abdbd72aa523ddc26c80a546
Anuradhadesilva <anuradhashashimal2@gmail.com>
2025-05-17 10:47:32 +0530

    create dev branch

--- lms-backend/src/main/java/com/example/lms_backend/Service/UserService.java
+++ lms-backend/src/main/java/com/example/lms_backend/Service/UserService.java
@@ -1,4 +1,13 @@
 package com.example.lms_backend.Service;
 
+import com.example.lms_backend.Repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
 }
+
