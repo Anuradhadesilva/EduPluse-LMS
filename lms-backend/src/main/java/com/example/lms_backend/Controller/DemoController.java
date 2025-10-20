5bbbe905c408498e2d0525e74adb204015cd4d2c
Anuradhadesilva <anuradhashashimal2@gmail.com>
2025-10-20 10:52:45 +0530

    add demo controller

---
+++ lms-backend/src/main/java/com/example/lms_backend/Controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.lms_backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DemoController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Backend API is running";
+    }
+
+}
