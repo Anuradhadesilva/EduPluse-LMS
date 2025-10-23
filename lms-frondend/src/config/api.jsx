79eb960a4173e4038956a061b88ffeabec5e8afb
Anuradhadesilva <anuradhashashimal2@gmail.com>
2025-10-23 14:20:56 +0530

    fixed origin error

--- lms-frondend/src/config/api.jsx
+++ lms-frondend/src/config/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5454"
+export const API_URL = "http://3.108.53.42:5454"
 
 export const api = axios.create({
     baseURL: API_URL,
